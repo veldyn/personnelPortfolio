e11d99a3c2297ea8cda790553ba03d191dc0f895
Veldyn <davidpeter072599@gmail.com>
2021-03-20 15:15:45 -0400

    made experimental section for testing purposes.

---
+++ experimental/login/login.php
@@ -0,0 +1,24 @@
+<?php
+include "connection.php";
+
+if(isset($_POST['but_submit'])){
+
+    $uname = mysqli_real_escape_string($con,$_POST['txt_uname']);
+    $password = mysqli_real_escape_string($con,$_POST['txt_pwd']);
+
+    if ($uname != "" && $password != ""){
+
+        $sql_query = "select count(*) as cntUser from users where username='".$uname."' and password='".$password."'";
+        $result = mysqli_query($con,$sql_query);
+        $row = mysqli_fetch_array($result);
+
+        $count = $row['cntUser'];
+
+        if($count > 0){
+            $_SESSION['uname'] = $uname;
+            header('Location: home.php');
+        }else{
+            echo "Invalid username and password";
+        }
+
+    }
