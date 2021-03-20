ff30d954a63ae5adc83fae0a62c14017957d113e
Veldyn <davidpeter072599@gmail.com>
2021-03-20 15:32:05 -0400

    changed database to 'users' from 'test'

--- accountCreation/connection.php
+++ accountCreation/connection.php
@@ -2,10 +2,6 @@
     $dbhost = 'localhost';
     $dbuser = 'root';
     $dbpass = 'p3rsonn3lp0rt4';
-    $db = 'test';
+    $db = 'users';
     $conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
 ?>
-
-
-
-
